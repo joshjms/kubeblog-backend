c07a8e4ff3f505fda9cf87bf0273d7f83bed677e
joshjms <joshjms1607@gmail.com>
2025-02-28 11:18:02 +0800

    feat: database conn

---
+++ database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbName, dbPort,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Database connected successfully!")
+
+	return db
+}
+
+func AutoMigrate(db *gorm.DB, models ...interface{}) {
+	if err := db.AutoMigrate(models...); err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+	log.Println("Database migration completed successfully!")
+}
