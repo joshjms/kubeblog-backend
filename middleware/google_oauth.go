c4c9652734b832a60e1f714efd0032022a6979c3
joshjms <joshjms1607@gmail.com>
2025-03-01 00:40:46 +0800

    feat: create user if not exists

--- middleware/google_oauth.go
+++ middleware/google_oauth.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/kubeblog/backend/database"
+	"github.com/kubeblog/backend/user"
 	"github.com/labstack/echo/v4"
 	"google.golang.org/api/idtoken"
 )
@@ -24,8 +27,23 @@ func ValidateGoogleTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 			return c.JSON(http.StatusUnauthorized, echo.Map{"error": "invalid token"})
 		}
 
-		// Store user information in the request context
-		c.Set("user", payload)
+		// Create user if not exists in database
+		userRepository := user.NewUserRepository(database.Db)
+		currentUser, err := userRepository.GetUserByEmail(payload.Claims["email"].(string))
+		if err != nil {
+			currentUser = &user.User{
+				ID:          uuid.New(),
+				Email:       payload.Claims["email"].(string),
+				Username:    payload.Claims["email"].(string),
+				DisplayName: payload.Claims["name"].(string),
+			}
+			if err := userRepository.CreateUser(currentUser); err != nil {
+				return c.JSON(http.StatusInternalServerError, echo.Map{"error": "failed to create user"})
+			}
+		}
+
+		// Store user in the request context
+		c.Set("user", currentUser)
 
 		return next(c)
 	}
