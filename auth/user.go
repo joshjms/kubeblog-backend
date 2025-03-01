19c8d9769ec2d169ac69c1d4d3bdd79540ea44cb
joshjms <joshjms1607@gmail.com>
2025-03-01 10:10:02 +0800

    feat: make username not null and unique

--- auth/user.go
+++ auth/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID          uuid.UUID `json:"id" gorm:"primaryKey"`
-	Username    string    `json:"username"`
+	Username    string    `json:"username" gorm:"unique;not null"`
 	Email       string    `json:"email"`
 	DisplayName string    `json:"display_name"`
 }
